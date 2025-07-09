418524b099e0d2f8b56ec95b9b6c7fb58cfe073b
sachin_sde <ssachin.s@myclnq.co.in>
2025-07-09 15:31:25 +0530

    s

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import FormTab from './tabFormComponent/FormTab';
+import Posts from './GraphQL/Posts';
 
 
 function App() {
   return (
     <div className="App">
      <h1>Tab Form</h1>
-     <FormTab />
+     
+     {/* <FormTab /> */}
+
+
+     <h1>GraphQL Integration</h1>
+     <Posts />
+
+
+
+
     </div>
   );
 }
